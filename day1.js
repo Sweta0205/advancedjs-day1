ef8ba0e004416fb3d3375836aeb6bfd2da90341a
sweta sriramadasu <swethasriramdas2311@gmail.com>
2022-06-08 10:38:30 +0200

    code

--- day1.js
+++ day1.js
@@ -1,33 +1,32 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
-		'X-RapidAPI-Key': 'fda1b99104msh4d7d14dcfdcef00p178727jsnb49e53870cb6'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
+        'X-RapidAPI-Key': 'fda1b99104msh4d7d14dcfdcef00p178727jsnb49e53870cb6'
+    }
 };
 
 fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem', options)
-	.then(response => response.json())
-	.then(response => console.log(response)
-    
-    
- )
-   
+    .then(response => response.json())
+    .then(response => console.log(response)
 
+
+    )
 
 fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=pink floyd', options)
-.then(response => response.json())
-.then(songList => {console.log(songList)
+    .then(response => response.json())
+    .then(songList => {
+        console.log(songList)
 
-const grid = document.querySelector(".container > .row")
+        const grid = document.querySelector(".container > .row")
 
-for (let i = 0; i < songList.data.length; i++) {
-    const song = songList.data[i]
+        for (let i = 0; i < songList.data.length; i++) {
+            const song = songList.data[i]
 
-    const col = document.createElement("div")
-    col.className = "col-sm-6 col-md-4 col-lg-3"
+            const col = document.createElement("div")
+            col.className = "col-sm-6 col-md-4 col-lg-3"
 
-    col.innerHTML = `
+            col.innerHTML = `
                 <div class="card">
                     <img src=${song.album.cover} class="card-img-top" alt=${song.album.title}>
                     <div class="card-body">
@@ -36,23 +35,25 @@ for (let i = 0; i < songList.data.length; i++) {
                 </div>
 `
 
-    grid.appendChild(col)
-}}
-)
-  
+            grid.appendChild(col)
+        }
+    }
+    )
+
 fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=daft punk', options)
-.then(response => response.json())
-.then(songList => {console.log(songList)
+    .then(response => response.json())
+    .then(songList => {
+        console.log(songList)
 
-const grid = document.querySelector(".container > .row")
+        const grid = document.querySelector(".container > .row")
 
-for (let i = 0; i < songList.data.length; i++) {
-    const song = songList.data[i]
+        for (let i = 0; i < songList.data.length; i++) {
+            const song = songList.data[i]
 
-    const col = document.createElement("div")
-    col.className = "col-sm-6 col-md-4 col-lg-3"
+            const col = document.createElement("div")
+            col.className = "col-sm-6 col-md-4 col-lg-3"
 
-    col.innerHTML = `
+            col.innerHTML = `
                 <div class="card">
                     <img src=${song.album.cover} class="card-img-top" alt=${song.album.title}>
                     <div class="card-body">
@@ -61,23 +62,25 @@ for (let i = 0; i < songList.data.length; i++) {
                 </div>
 `
 
-    grid.appendChild(col)
-}}
-)
-  
+            grid.appendChild(col)
+        }
+    }
+    )
+
 fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=metallica', options)
-.then(response => response.json())
-.then(songList => {console.log(songList)
+    .then(response => response.json())
+    .then(songList => {
+        console.log(songList)
 
-const grid = document.querySelector(".container > .row")
+        const grid = document.querySelector(".container > .row")
 
-for (let i = 0; i < songList.data.length; i++) {
-    const song = songList.data[i]
+        for (let i = 0; i < songList.data.length; i++) {
+            const song = songList.data[i]
 
-    const col = document.createElement("div")
-    col.className = "col-sm-6 col-md-4 col-lg-3"
+            const col = document.createElement("div")
+            col.className = "col-sm-6 col-md-4 col-lg-3"
 
-    col.innerHTML = `
+            col.innerHTML = `
                 <div class="card">
                     <img src=${song.album.cover} class="card-img-top" alt=${song.album.title}>
                     <div class="card-body">
@@ -86,20 +89,21 @@ for (let i = 0; i < songList.data.length; i++) {
                 </div>
 `
 
-    grid.appendChild(col)
-}}
-)
-.catch(err => console.error(err));
-  
-        
-    
+            grid.appendChild(col)
+        }
+    }
+    )
+    .catch(err => console.error(err));
 
 
 
 
 
 
-    
+
+
+
+
 
 
 
